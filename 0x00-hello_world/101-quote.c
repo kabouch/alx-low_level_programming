dda77aafa773116648b5a4801eb0db3bb2c8a20d
kabouch <abdellatif.kabouch@gmail.com>
2023-03-16 12:37:20 -0700

    New

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: 0
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
