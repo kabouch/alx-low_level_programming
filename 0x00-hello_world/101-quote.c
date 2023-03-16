e01229b43cf4bdb421134e4a3e1b52b2abb2c480
kabouch <abdellatif.kabouch@gmail.com>
2023-03-16 12:48:38 -0700

    New

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,11 @@
  * main - This is the main function
  * Return: 0
  */
+
+
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
