eade4db50a5ca53f040fd0c7a16931293ced7ed1
kabouch <abdellatif.kabouch@gmail.com>
2023-03-17 12:43:26 -0700

    New

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+		putchar(n+32);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
