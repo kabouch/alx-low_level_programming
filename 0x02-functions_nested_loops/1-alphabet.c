10fb8828e2b4ba2bbaa4a04de9f379e2e0f2aed1
kabouch <abdellatif.kabouch@gmail.com>
2023-03-21 05:49:01 -0700

    New

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_alphabet - Prints alphabets in lower case
+ * Return:0
+ */
+void print_alphabet(void);
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
