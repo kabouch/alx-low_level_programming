e37cc12c5a7a81bb1d29ef3ac11883318a33fe8c
kabouch <abdellatif.kabouch@gmail.com>
2023-03-21 08:47:48 -0700

    New

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,26 @@ int print_sign(int n)
 	if (n > 0 )
 	{
 		putchar(43);
+		putchar(49);
 		/*putchar(',');
-		putchar(' ');*/
-		return (1);
+		putchar(' ');
+		return (1);*/
 	}
 	else if (n == 0)
 	{
 		putchar(48);
+		putchar(48);
                /* putchar(',');
-                putchar(' ');*/
-		return (0);
+                putchar(' ');
+		return (0);*/
 	}
 	else
 	{
 		putchar(45);
+		putchar(49);
                /* putchar(',');
-                putchar(' ');*/
-                return (-1);
+                putchar(' ');
+                return (-1);*/
 	}
 	putchar('\n');
 }
