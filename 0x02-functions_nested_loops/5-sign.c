1dc268f416c5e488a8e30605aba35558b2fd0c09
kabouch <abdellatif.kabouch@gmail.com>
2023-03-21 08:38:37 -0700

    New

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_sign - X
+ * @n: X
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > )
+	{
+		putchar(43);
+		putchar(',');
+		putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+                putchar(',');
+                putchar(' ');
+		return (0)
+	}
+	else
+	{
+		putchar(45);
+                putchar(',');
+                putchar(' ');
+                return (-1)
+	}
+	putchar('\n');
+}
