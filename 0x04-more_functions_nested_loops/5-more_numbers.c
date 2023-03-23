6d2719ed05727d05bbd35dba1df383ad1426588e
kabouch <abdellatif.kabouch@gmail.com>
2023-03-23 10:24:28 -0700

    New

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - X
+ * @void: void
+ */
+
+void more_numbers(void)
+{
+	int line = 1;
+	int num;
+
+	while (line <= 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+		line++;
+	}
+	_putchar('\n');
+}
