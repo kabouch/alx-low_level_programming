2291937759e11a96ec832148fc49c25ad69ff1db
kabouch <abdellatif.kabouch@gmail.com>
2023-03-23 10:40:26 -0700

    New

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			_putchar(num + '0');
+			_putchar((num / 10) + '0');
 		}
-		_putchar('\n');
+		_putchar((num % 10) + '0');
 		line++;
 	}
 	_putchar('\n');
