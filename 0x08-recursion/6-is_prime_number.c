732e622619e7f5c7fe79bf032178a836039b5a51
kabouch <abdellatif.kabouch@gmail.com>
2023-04-05 11:42:14 -0700

    New

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * check_prime - X
+ * @n: n
+ * @i: i
+ */
+
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	
+	if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+
+	if ((n / i) < i)
+	{
+		return (1);
+	}
+
+	return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - X
+ * @n: n
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
