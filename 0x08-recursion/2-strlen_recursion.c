dffa75f966892666e734e172f90eb12baaf70326
kabouch <abdellatif.kabouch@gmail.com>
2023-04-05 10:54:35 -0700

    New

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s == '\0')
 	{
-		return (len);
+		return (0);
 	}
 
-	len++;
-	_strlen_recursion(s);
+	s++;
+	return (_estrlen_renursion(s) + 1);
 }
