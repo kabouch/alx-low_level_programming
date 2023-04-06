9d4cb555311c5e9e8a2c7980870d92e0defa2b92
kabouch <abdellatif.kabouch@gmail.com>
2023-04-06 04:00:22 -0700

    New

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome - X
+ * @s: s
+ * Return: int
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s = '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
