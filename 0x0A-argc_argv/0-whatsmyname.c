7104bb54714840f3a60caef25b536daf79ba72c6
kabouch <abdellatif.kabouch@gmail.com>
2023-04-10 10:16:01 -0700

    New

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - X
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
