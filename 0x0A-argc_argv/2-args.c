f2565940b22d42254e4e96a2e23a4bcc3d385cef
kabouch <abdellatif.kabouch@gmail.com>
2023-04-10 11:08:11 -0700

    New

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - X
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i > argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
